d2faa66e6fc455da8b9d9abdc7fa50c4d935e52d
Raph1105 <Raph1105@hotmail.com>
2018-03-20 23:18:05 -0400

    Fixed issue : multiple choice in the preferences; show those multiple categories in the discovery page

--- app/src/main/java/de/danoeh/antennapod/fragment/DiscoveryFragment.java
+++ app/src/main/java/de/danoeh/antennapod/fragment/DiscoveryFragment.java
@@ -139,48 +139,51 @@ public class DiscoveryFragment extends ItunesSearchFragment {
             }
 
             CategoryId = discoveryIds;
+            List<ItunesAdapter.Podcast> results = new ArrayList<>();
 
-            for(int b = 0; b < discoveryIds.size(); b++){
 
-                int getID = discoveryIds.get(b);
 
                 subscription = Observable.create((Observable.OnSubscribe<List<ItunesAdapter.Podcast>>) subscriber -> {
-                    String lang = Locale.getDefault().getLanguage();
-                    String url = "https://itunes.apple.com/" + lang + "/rss/toppodcasts/limit=25/genre=" + getID + "/json";
-                    OkHttpClient client = AntennapodHttpClient.getHttpClient();
-                    Request.Builder httpReq = new Request.Builder()
-                            .url(url)
-                            .header("User-Agent", ClientConfig.USER_AGENT);
-                    List<ItunesAdapter.Podcast> results = new ArrayList<>();
-                    try {
-                        Response response = client.newCall(httpReq.build()).execute();
-                        if(!response.isSuccessful()) {
-                            // toplist for language does not exist, fall back to united states
-                            url = "https://itunes.apple.com/us/rss/toppodcasts/limit=25/genre=" + getID + "/json";
-                            httpReq = new Request.Builder()
-                                    .url(url)
-                                    .header("User-Agent", ClientConfig.USER_AGENT);
-                            response = client.newCall(httpReq.build()).execute();
-                        }
-                        if(response.isSuccessful()) {
-                            String resultString = response.body().string();
-                            //System.out.println(resultString);
-                            JSONObject result = new JSONObject(resultString);
-                            JSONObject feed = result.getJSONObject("feed");
-                            JSONArray entries = feed.getJSONArray("entry");
+                    for(int b = 0; b < discoveryIds.size(); b++) {
 
-                            for(int i = 0; i < entries.length(); i++) {
-                                JSONObject json = entries.getJSONObject(i);
-                                ItunesAdapter.Podcast podcast = ItunesAdapter.Podcast.fromToplist(json);
-                                results.add(podcast);
+                        int getID = discoveryIds.get(b);
+
+                        String lang = Locale.getDefault().getLanguage();
+                        String url = "https://itunes.apple.com/" + lang + "/rss/toppodcasts/limit=25/genre=" + getID + "/json";
+                        OkHttpClient client = AntennapodHttpClient.getHttpClient();
+                        Request.Builder httpReq = new Request.Builder()
+                                .url(url)
+                                .header("User-Agent", ClientConfig.USER_AGENT);
+                        try {
+                            Response response = client.newCall(httpReq.build()).execute();
+                            if (!response.isSuccessful()) {
+                                // toplist for language does not exist, fall back to united states
+                                url = "https://itunes.apple.com/us/rss/toppodcasts/limit=25/genre=" + getID + "/json";
+                                httpReq = new Request.Builder()
+                                        .url(url)
+                                        .header("User-Agent", ClientConfig.USER_AGENT);
+                                response = client.newCall(httpReq.build()).execute();
                             }
+                            if (response.isSuccessful()) {
+                                String resultString = response.body().string();
+                                //System.out.println(resultString);
+                                JSONObject result = new JSONObject(resultString);
+                                JSONObject feed = result.getJSONObject("feed");
+                                JSONArray entries = feed.getJSONArray("entry");
+
+                                for (int i = 0; i < 10; i++) {
+                                    JSONObject json = entries.getJSONObject(i);
+                                    ItunesAdapter.Podcast podcast = ItunesAdapter.Podcast.fromToplist(json);
+                                    results.add(podcast);
+                                }
+                            } else {
+                                String prefix = getString(R.string.error_msg_prefix);
+                                subscriber.onError(new IOException(prefix + response));
+                            }
+                        } catch (IOException | JSONException e) {
+                            subscriber.onError(e);
                         }
-                        else {
-                            String prefix = getString(R.string.error_msg_prefix);
-                            subscriber.onError(new IOException(prefix + response));
-                        }
-                    } catch (IOException | JSONException e) {
-                        subscriber.onError(e);
+
                     }
                     subscriber.onNext(results);
                     subscriber.onCompleted();
@@ -201,7 +204,7 @@ public class DiscoveryFragment extends ItunesSearchFragment {
                         });
             }
 
-    }
+
 
     public static List<Integer> getCategoryId() { return CategoryId; }
 
