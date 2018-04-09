5a0001ffec70066efc9da3ca240c3b40a7a450e5
Sarb <sarbeng4@hotmail.com>
2018-04-09 19:01:29 -0400

    Created home page in menue & added icon.

---
+++ app/src/main/java/de/danoeh/antennapod/fragment/HomeFragment.java
@@ -0,0 +1,14 @@
+package de.danoeh.antennapod.fragment;
+
+public class HomeFragment extends ItunesSearchFragment{
+
+    public static final String TAG = "HomeFragment";
+
+
+    /**
+     * Constructor
+     */
+    public HomeFragment() {
+        // Required empty public constructor
+    }
+}
